98e850fc782702d6c009e5de4c47cf1dc152dd8d
pratyaksh mundra <pratyaksh.mundra@live.com>
2021-11-20 18:18:38 +0530

    first page made

--- first_Page.py
+++ first_Page.py
@@ -3,16 +3,29 @@ import os
 from tkinter import *
 
 window = Tk()
+window.configure(bg="#E9C46A")
 
 window.title("Running Python Script")
 window.geometry('550x200')
 
-def run():
-    os.system('python manager.py')
 
-btn = Button(window, text="MANAGER VIEW", bg="black", fg="white",command=run)
-btn = Button(window, text="EMPLOYEE VIEW", bg="black", fg="white",command=run)
-btn = Button(window, text="CUSTOMER VIEW", bg="black", fg="white",command=run)
-btn.grid(column=0, row=0)
+def run_manager():
+    os.system('python manager.py')
+def run_employee():
+    os.system('python employee.py')
+def run_member():
+    os.system('python member.py')
+
+
+# region misc
+btn1 = Button(window, text="MANAGER VIEW", bg="#264653", fg="#2A9D8F", command=run_manager)
+btn2 = Button(window, text="EMPLOYEE VIEW", bg="#264653", fg="#2A9D8F", command=run_employee)
+btn3 = Button(window, text="MEMBER VIEW", bg="#264653", fg="#2A9D8F", command=run_member)
+btn1.grid(column=0, row=0)
+empty=Label(window, bg="#E9C46A").grid(row=1, column=0)
+btn2.grid(column=0, row=2)
+empty=Label(window, bg="#E9C46A").grid(row=3, column=6)
+btn3.grid(column=0, row=4)
+# endregion
 
 window.mainloop()
